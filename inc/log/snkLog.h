922dd835b077cf57803e7a525dd0219a3695d20e
Alexander Orel <desperius@gmail.com>
2016-09-05 11:51:11 +0300

    Doxygen documentatin settings were added

--- inc/log/snkLog.h
+++ inc/log/snkLog.h
@@ -1,12 +1,26 @@
+/**
+ *   @file      snkLog.h
+ *   @brief     File contains functions and macros for debugging
+ *   @author    Alexander Orel (desperius@gmail.com)
+ *   @version   1.0
+ *   @date      05.09.2016
+ *   @copyright GNU Public License
+ */
+
 #ifndef _SNK_LOG_H_
 #define _SNK_LOG_H_
 
 #include <iostream>
 
+/**
+ *   @def LOG(x)
+ *   Prints debug output in debug mode. Prints nothing in release mode.
+ */
+
 #ifdef  DEBUG
 #define LOG(x) do { std::cout << x << std::endl; } while(0)
 #else
 #define LOG(x)
-#endif
+#endif /* DEBUG */
 
 #endif /* _SNK_LOG_H_ */
--- inc/main.h
+++ inc/main.h
@@ -1,3 +1,12 @@
+/**
+ *   @file      main.h
+ *   @brief     Contains all needed includes
+ *   @author    Alexander Orel (desperius@gmail.com)
+ *   @version   1.0
+ *   @date      05.09.2016
+ *   @copyright GNU Public License
+ */
+
 #ifndef _SNK_MAIN_H_
 #define _SNK_MAIN_H_
 
